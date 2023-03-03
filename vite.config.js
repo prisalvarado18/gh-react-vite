4c67a0ef5867381c472062cba9ad5960a5f258c2
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-03-03 12:55:32 -0500

    Added root

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://prisalvarado18.github.io/gh-react-vite',
+  base: 'https://prisalvarado18.github.io/gh-react-vite/',
   plugins: [react()],
 })
