936053ee862771aa76adec8a6935f12e8081eb2f
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-03-06 19:40:57 -0500

    Added components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+console.log('Lalala')
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/components/helloWorld.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function helloWorld() {
+  return (
+    <div>Ehhhhhhh</div>
+  )
+}
+
+export default helloWorld
