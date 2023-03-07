8beaafe772238f16168365ae10f535f171c0768c
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-03-06 20:22:21 -0500

    Imported the recommended router for all React Router web projects

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, createBrowserRouter, RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
 )
