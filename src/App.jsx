96b297e397c704b904bd013da5e24434ca75d5d9
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-03-07 23:32:49 -0500

    Removed /auth

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/gh-react-vite/" element={<Login />} />
+      <Route path="/gh-react-vite/" element={<Login />} /> 
     </Routes>
 
   )
