68edc0beff54ecc84f44380e5ca20555828833a8
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-03-10 14:59:18 -0500

    Added logo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <img src="./src/assets/cara_logo_white.png" class='logo-header' alt="cara logo white"/> 
+      <img src="https://raw.githubusercontent.com/prisalvarado18/gh-react-vite/main/src/assets/cara_logo_white.png" class='logo-header' alt="cara logo white"/> 
     </header>
   );
 };
