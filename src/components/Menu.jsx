42e34990af6448be82f6e00f2952f5830f3c67e8
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-03-13 23:50:12 -0500

    Added dynamism to the menu button

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -22,29 +22,29 @@ const Menu = () => {
   return (
     <>
       <LoggedinHeader />
-      <button className="menu-button" onClick={toggleMenu}>
-        Menu
+      <button className={`menu-button ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <img className={isOpen ? 'move-left' : ''} src="./src/assets/cutlery-icon.png" alt="cutlery icon" />
       </button>
       <nav className={`menu ${isOpen ? 'open' : ''}`}>
         <ul>
           <li>
-            <img src="./src/assets/users-icon.png" className='logo-header' alt="users icon" />
+            <img src="./src/assets/users-icon.png" alt="users icon" />
             <Link to="/gh-react-vite/users">Users</Link>
           </li>
           <li>
-            <img src="./src/assets/products-icon.png" className='logo-header' alt="products icon" />
+            <img src="./src/assets/products-icon.png" alt="products icon" />
             <Link to="/gh-react-vite/products">Products</Link>
           </li>
           <li>
-            <img src="./src/assets/orders-icon.png" className='logo-header' alt="orders icon" />
+            <img src="./src/assets/orders-icon.png" alt="orders icon" />
             <Link to="/gh-react-vite/orders">Orders</Link>
           </li>
           <li>
-            <img src="./src/assets/status-icon.png" className='logo-header' alt="status icon" />
+            <img src="./src/assets/status-icon.png" alt="status icon" />
             <Link to="/gh-react-vite/status">Status</Link>
           </li>
           <li>
-            <img src="./src/assets/profile-icon.png" className='logo-header' alt="profile icon" />
+            <img src="./src/assets/profile-icon.png" alt="profile icon" />
             <Link to="/gh-react-vite/profile">Profile</Link>
           </li>
           <li>
