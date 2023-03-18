faa7f1187481c46f8e0cb2a8156ba6e4bc7c9272
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-03-18 12:01:22 -0500

    Looped through Lunch Products

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -16,7 +16,7 @@ const Orders = () => {
     let headers = { Authorization: `Bearer ${token}` };
 
     if (!productsObtained) {
-      axios.get('https://palvaradoristorante.onrender.com/products?limit=4&page=1', { headers })
+      axios.get('https://palvaradoristorante.onrender.com/products?limit=10&page=1', { headers })
         .then((response) => {
           console.log(response);
           setProducts(response.data);
@@ -28,11 +28,12 @@ const Orders = () => {
     }
   }, [productsObtained]);
 
-  const breakfastList = () => {
+  const lunchList = () => {
+    console.log(products)
     if (products && products.length) {
-      const breakfast = products.filter((product) => product.type === 'Brunch');
-      console.log(breakfast);
-      setProductsType(breakfast);
+      const lunch = products.filter((product) => product.type === 'Lunch');
+      console.log(lunch);
+      setProductsType(lunch);
     }
   }
 
@@ -72,14 +73,118 @@ const Orders = () => {
               <tbody>
                 <tr>
                   <td className="border-bottom-btn">
-                    <button className="type-btn" onClick={breakfastList}>
+                    <button className="type-btn">
                       <p>BREAKFAST</p>
                     </button>
                   </td>
                 </tr>
                 <tr>
                   <td className="border-bottom-btn">
+                    <button className="type-btn" onClick={lunchList}>
+                      <p>LUNCH</p>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border-bottom-btn">
                     <button className="type-btn">
+                      <p>DINNER</p>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <button className="type-btn">
+                      <p>DRINKS</p>
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div className="product-selection">
+            <div className="product-row-first">
+              {productsType.slice(0, 2).map((product, index) => (
+                <div className="product-item" key={index}>
+                  <button>
+                    <figure className="image-container">
+                      <img src={product.image} alt={product.name} />
+                    </figure>
+                    <p className="product-description">{product.name.toUpperCase()}</p>
+                    <p className="product-price">{product.price}</p>
+                  </button>
+                </div>
+              ))}
+            </div>
+            <div className="product-row-second">
+              {productsType.slice(2, 4).map((product, index) => (
+                <div className="product-item" key={index}>
+                  <button>
+                    <figure className="image-container">
+                      <img src={product.image} alt={product.name} />
+                    </figure>
+                    <p className="product-description">{product.name.toUpperCase()}</p>
+                    <p className="product-price">{product.price}</p>
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default Orders;
+
+/*
+
+  return (
+    <>
+      <Menu />
+      <section className="orders-body">
+        <section className="client-name">
+          <input type="text" placeholder="client name" required />
+        </section>
+        <section className="orders-container">
+          <table className="product-table">
+            <thead>
+              <tr>
+                <th>PRODUCT</th>
+                <th>PRICE</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Chosen Product 1</td>
+                <td>$1</td>
+              </tr>
+              <tr>
+                <td>Chosen Product 2</td>
+                <td>$2</td>
+              </tr>
+              <tr>
+                <td>Chosen Product 3</td>
+                <td>$3</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div className="menu-buttons">
+            <table>
+              <tbody>
+                <tr>
+                  <td className="border-bottom-btn">
+                    <button className="type-btn">
+                      <p>BREAKFAST</p>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border-bottom-btn">
+                    <button className="type-btn" onClick={lunchList}>
                       <p>LUNCH</p>
                     </button>
                   </td>
@@ -115,6 +220,7 @@ const Orders = () => {
               </div>
               <div className="product-item">
                 <button>
+                  
                   <figure className="image-container">
                     <img src="src/assets/product-summer-rolls.png" alt="Product 2" />
                   </figure>
@@ -151,3 +257,4 @@ const Orders = () => {
 };
 
 export default Orders;
+*/
