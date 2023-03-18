a31dc8a82ecd3cba92ccddce7164ff1cd9fc7c99
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-03-18 15:47:29 -0500

    Fixing Header

--- src/components/LoggedinHeader.jsx
+++ src/components/LoggedinHeader.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Menu from "./Menu";
 import '../styles/LoggedinHeader.css';
+import CaraLogoBlack from "../assets/cara_logo_black.png";
 
 const LoggedinHeader = () => {
     return (
         <header className='header-loggedin'>
-            <img src="./src/assets/cara_logo_black.png" className='logo-header-loggedin' alt="cara logo black" />
+            <img src={CaraLogoBlack} className='logo-header-loggedin' alt="cara logo black" />
         </header>
     );
 };
