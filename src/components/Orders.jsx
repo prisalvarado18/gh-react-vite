a8d4ec3cfb527f0ea2b8cbbbebf417057f52db14
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-07-03 22:06:23 -0500

    Added orders component

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import OtroHeader from "./OtroHeader";
 import LoggedinHeader from "./LoggedinHeader";
 import '../styles/Orders.css';
+import ModalDelivered from "./ModalDelivered";
+import ModalCancelled from "./ModalCancelled";
 
 const Orders = () => {
   const [products, setProducts] = useState([]);
@@ -132,7 +134,7 @@ const Orders = () => {
     /*console.log(order);*/
     axios.post(url, order, { headers })
       .then(() => {
-        document.querySelector('.inputName').value = '';
+        document.querySelector('.input-name').value = '';
         setItemsCart([]);
         setSelectedItems([]);
         setTotalFee(0)
@@ -142,16 +144,98 @@ const Orders = () => {
       })
   }
 
+  // Get Orders
+  const [infoOrders, setInfoOrders] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    let headers = { Authorization: `Bearer ${token}` };
+    /*const cacheData = localStorage.getItem('infoOrders');*/
+
+    /*if (cacheData) {
+      setInfoOrders(JSON.parse(cacheData));
+    } else {*/
+    axios.get('https://palvaradoristorante.onrender.com/orders', { headers })
+      .then((response) => {
+        const filteredOrders = response.data.filter((order) => order.status === 'pending' || order.status === 'delivering');
+        setInfoOrders(filteredOrders);
+        /*localStorage.setItem('infoOrders', JSON.stringify(filteredOrders));*/
+      })
+      .catch((error) => console.error(error));
+    //}
+  }, []);
+
+  // *** * *** * *** * *** * *** * *** * *** * *** * *** * *** * *** * *** * *** * *** * *** * *** * ***
+  const [show, setShow] = useState(true);
+  const [ modalOpenStatus, setModalOpenStatus ] = useState(false);
+  
+  const openTheModal = () => {
+    setModalOpenStatus(true);
+  }
+
+  const closeTheModal = () => {
+    setModalOpenStatus(false);
+  }
+
+  const [orderId, setOrderId] = useState('');
+
+  const chooseOrder = (order) => {
+    setOrderId(order._id);
+  }
+
+  const updateAsDelivered = () => {
+    const token = localStorage.getItem('token');
+    let headers = { Authorization: `Bearer ${token}` };
+    const url = `https://palvaradoristorante.onrender.com/orders/${orderId}`;
+
+    const statusUpdate = {
+      status: 'delivered'
+    }
+
+    axios.put(url, statusUpdate, {headers}).then((response) => {
+      console.log(response);
+      closeTheModal();
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+  
+  //* * * * * * * * *  ** * * * * * * * * * * * 
+  const [modalCancelledStatus, setModalCancelledStatus] = useState(false);
+  const openTheCanceledModal = () => {
+    setModalCancelledStatus(true);
+}
+
+const closeTheCanceledModal = () => {
+  setModalCancelledStatus(false);
+}
+
+const updateStatusToCanceled = () => {
+  const token = localStorage.getItem('token');
+  let headers = { Authorization: `Bearer ${token}` };
+  const url = `https://palvaradoristorante.onrender.com/orders/${orderId}`;
+  const statusUpdate = {
+    status: 'cancelled'
+  }
+  axios.put(url, statusUpdate, {headers}).then((response) => {
+    console.log(response);
+    closeTheCanceledModal();
+  }).catch((error) => {
+    console.info(error);
+  })
+}
+
+
   return (
     <>
       <Menu />
       <section className="orders-main">
         <section className="orders-body">
-          <section className="client-name">
-            <input type="text" placeholder="client name" name="client" className="inputName" onChange={setClientData} required />
+          <section className="client-name" style={{ display: show ? "block" : "none" }}>
+            <input type="text" placeholder="client name" name="client" className="input-name" onChange={setClientData} required />
           </section>
           <section className="orders-container">
-            <section className="product-table-btns-container">
+            <section className="product-table-btns-container" style={{ display: show ? "block" : "none" }}>
               <table className="product-table">
                 <thead>
                   <tr>
@@ -176,11 +260,11 @@ const Orders = () => {
               </table>
               <section className="view-send-container" >
                 <button className="send-btn" onClick={saveOrderToDatabase}>SEND</button>
-                <button className="view-all-btn"> VIEW ALL</button>
+                <button className="view-all-btn" onClick={() => setShow((shouldShow) => !shouldShow)}> VIEW ALL</button>
               </section>
             </section>
 
-            <div className="menu-buttons">
+            <div className="menu-buttons" style={{ display: show ? "block" : "none" }}>
               <table>
                 <tbody>
                   <tr>
@@ -215,7 +299,7 @@ const Orders = () => {
               </table>
             </div>
 
-            <div className="product-selection">
+            <div className="product-selection" style={{ display: show ? "block" : "none" }}>
               <div className="product-row-first">
                 {productsType.slice(0, 2).map((product, index) => (
                   <div className="product-item" key={index}>
@@ -244,6 +328,46 @@ const Orders = () => {
               </div>
             </div>
           </section>
+
+
+          <div className="orders-list-container" style={{ display: show ? "none" : "block" }}>
+            <button className='back-btn' onClick={() => setShow((shouldShow) => !shouldShow)}> BACK </button>
+            {
+              infoOrders.map((order, index) => {
+                return (
+                  <div className="container-cards-orders" key={index}>
+                    <div className="card-orders" style={order.status === "delivering" ? { border: "solid #800020" } : { border: "none" }}>
+                      <p className="client-name"> <span>CLIENT:</span> {order.client.toUpperCase()} </p>
+                      <div className="table-order-container">
+                        <table className="order-table">
+                          <tbody>
+                            {order.products.map((item, index) => {
+                              return (
+                                <tr key={index}>
+                                  <td className="order-info-name">{item.productId.name.toUpperCase()}</td>
+                                  <td className="order-info-qty">{item.qty}</td>
+                                  <td className="order-info-price">$ {item.productId.price}</td>
+                                </tr>
+                              );
+                            })}
+                          </tbody>
+                        </table>
+                      </div>
+
+                      <div className="delete-check-btns">
+                        <p className="order-status"> STATUS: <span className="status-description">{order.status.toUpperCase()}</span>  </p>
+                        <div className="buttons">
+                          <button className="delete-btn" onClick={()=> {chooseOrder(order); openTheCanceledModal()}}>Trash</button>
+                          <button className="check-btn" onClick={()=> {chooseOrder(order); openTheModal()}}>check</button>
+                          {modalOpenStatus && <ModalDelivered setOpenModal={closeTheModal} handleUpdate={updateAsDelivered} />}
+                          {modalCancelledStatus && <ModalCancelled setOpenModal={closeTheCanceledModal} handleUpdate={updateStatusToCanceled} />}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
         </section>
       </section>
     </>
--- src/components/helloWorld.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function helloWorld() {
-  return (
-    <div>Ehhhhhhh</div>
-  )
-}
-
-export default helloWorld
